bae4e755296ffd1239025d1feed1cf39cc471e70
Sergei Malafeev <sergeymalafeev@gmail.com>
2020-03-27 16:45:31 +0800

    update
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,12 @@
 import io.opentelemetry.OpenTelemetry;
 import io.opentelemetry.exporters.logging.LoggingMetricExporter;
 import io.opentelemetry.metrics.LongCounter;
+import io.opentelemetry.metrics.LongCounter.BoundLongCounter;
 import io.opentelemetry.metrics.Meter;
 import io.opentelemetry.sdk.metrics.export.IntervalMetricReader;
 import io.opentelemetry.sdk.metrics.export.MetricProducer;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class App {
   public static void main(String[] args) {
@@ -17,9 +19,23 @@ public class App {
             .setMetricProducers(new ArrayList<MetricProducer>())
             .build();
 
-    final Meter meter = OpenTelemetry.getMeterProvider().get("test");
-    final LongCounter longCounter = meter.longCounterBuilder("long").setUnit("123").build();
-    longCounter.add(321);
+    // Gets or creates a named meter instance
+    Meter meter = OpenTelemetry.getMeterProvider()
+        .get("instrumentation-library-name", "semver:1.0.0");
+
+    // Build counter e.g. LongCounter
+    LongCounter counter = meter
+        .longCounterBuilder("processed_jobs")
+        .setDescription("Processed jobs")
+        .setUnit("1")
+        .setLabelKeys(Collections.singletonList("Key"))
+        .build();
+
+    // It is recommended that the API user keep a reference to a Bound Counter
+    BoundLongCounter someWorkCounter = counter.bind("Key", "SomeWork");
+
+    // Record data
+    someWorkCounter.add(123);
 
 
   }
